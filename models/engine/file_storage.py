a82c87af5dff0f1c459d55386d12571f32b4f9db
DarianGrabino <dariangra@gmail.com>
2023-08-01 09:39:15 -0700

    add def delete and update def all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls != None:
+            objs = {}
+            for key, value in self.__objects.items():
+                if isinstance(value, cls):
+                    objs[key] = value
+            return objs
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +55,8 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete object"""
+        if obj != None and obj.__class__.__name__ + "." + obj.id in self.__objects:
+            del self.__objects[obj.__class__.__name__ + "." + obj.id]
