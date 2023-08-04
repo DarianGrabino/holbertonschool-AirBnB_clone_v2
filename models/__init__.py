2df729f892ecd455af81f16fed63935e33302861
DarianGrabino <dariangra@gmail.com>
2023-08-04 13:33:43 -0700

    update conditional

--- models/__init__.py
+++ models/__init__.py
@@ -6,13 +6,8 @@ from models.engine.file_storage import FileStorage
 from os import getenv
 
 
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
-
-if storage_type == "db":
-    from models.engine.db_storage import DBStorage
+if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
