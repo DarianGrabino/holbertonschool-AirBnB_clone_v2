00db8a2625da33793b1152d9a97f30f5211ddd03
DarianGrabino <dariangra@gmail.com>
2023-08-04 12:50:22 -0700

    update, add DBStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ create a unique FileStorage instance for your application """
 
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+from os import getenv
 
-from .engine.file_storage import FileStorage
 
-storage = FileStorage()
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+if storage_type == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
