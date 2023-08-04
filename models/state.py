18cd119dd66c972c56a9d512b52e05f315e46279
DarianGrabino <dariangra@gmail.com>
2023-08-04 09:47:29 -0700

    add class attribute and method cities

--- models/state.py
+++ models/state.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 """Defines the class State that inherit from BaseModel"""
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """Creating an empty State class"""
 
-    name = ""
+    
+    __tablename__ = 'states'
+    name = Column(String(128), nullable = False)
+    cities = relationship("City", backref = "state", cascada = "all, delete")
+
+    def cities(self):
+        """Return the list of cities associated with the current state"""
+        from models import storage
+        from models.city import City
+        asociated_cities = []
+        all_cities = storage.all(City)
+        for city in all_cities.values():
+            if city.state_id == self.id:
+                asociated_cities.append(city)
+        return asociated_cities
