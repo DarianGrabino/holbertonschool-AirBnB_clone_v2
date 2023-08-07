9633488482211fd21ebed3c23ecacd65115f8dfd
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-08-07 00:00:21 -0300

    Task 8

--- models/user.py
+++ models/user.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Defines the class User that inherit from BaseModel"""
 
-from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
 class User(BaseModel):
     """Creates a new empty user"""
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
