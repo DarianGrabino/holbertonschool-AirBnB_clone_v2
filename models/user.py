bce20dde9fecb533a9c1f9b86e1445b32a79c383
DarianGrabino <dariangra@gmail.com>
2023-08-06 21:12:25 -0700

    add relationship

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """Creates a new empty user"""
@@ -13,3 +14,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
